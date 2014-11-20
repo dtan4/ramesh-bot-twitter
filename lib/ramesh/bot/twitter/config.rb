8d713953c8b5a58f47aacca36fca5a4a753f4ece
Daisuke Fujita <dtanshi45@gmail.com>
2014-11-21 02:00:35 +0900

    Create Config class

---
+++ lib/ramesh/bot/twitter/config.rb
@@ -0,0 +1,21 @@
+module Ramesh
+  module Bot
+    class Twitter
+      class Config
+        KEYS = %i(consumer_key consumer_secret access_token access_token_secret white_list)
+
+        KEYS.each do |key|
+          define_method(k) { @options[k] }
+        end
+
+        DEFAULTS = {
+          white_list: []
+        }
+
+        def initialize(options = {})
+          @options = DEFAULTS.merge(options)
+        end
+      end
+    end
+  end
+end
